9852583686becaafed45c2bdc0f57be8ad09d855
Shantnav Agarwal <shantnavagarwal@gmail.com>
2024-05-30 13:55:52 +0000

    add mission planner cpp

--- src/mission_planner/src/mission_planner.py
+++ src/mission_planner/src/mission_planner.py
@@ -1,7 +1,9 @@
 import rospy
 from std_msgs.msg import path
-from geometry_msgs.msg import PoseStamped
-
+from geometry_msgs.msg import PoseStamped, PoseWithCovariance
+from custom_msgs.msg import ObjectLocationArray
+from sensor_msgs.msg import BatteryState
+from action import Move
 
 class MissionPlanner:
     def __init__(self):
@@ -12,4 +14,14 @@ class MissionPlanner:
         self.status = None
         self.current_goal = None
 
-        self.sub_obj_track = rospy.Subscriber('')
+        self.sub_obj_track = rospy.Subscriber('/tracker/feces_locations', ObjectLocationArray, self.obj_track_callback)
+        self.sub_own_location = rospy.Subscriber('/acml_pose', PoseWithCovariance, self.own_location_callback)
+        self.sub_battery = rospy.Subscriber('/mirte/power/power_watcher', BatteryState, self.battery_callback)
+
+        self.pub_goal = rospy.Publisher('')
+
+    def obj_track_callback(self, msg):
+        pass
+
+    def own_location_callback(self, data):
+        pass
