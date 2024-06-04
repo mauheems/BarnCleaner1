bfd38f2620a499cc2ba19ddf6f43b0f052906790
tvennink <t.vennink@student.tudelft.nl>
2024-06-04 10:32:08 +0200

    first iteration of function to divide among robots and create waypoints in map in zigzag pattern

--- global_mission_planner/scripts/global_mission_planner_node.py
+++ global_mission_planner/scripts/global_mission_planner_node.py
@@ -2,7 +2,8 @@
 import rospy
 from nav_msgs.msg import OccupancyGrid
 from geometry_msgs.msg import PoseArray
-from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import Pose
+from tf.transformsudo apt-get install ros-<distro>-geometry-msgsations import euler_from_quaternion
 
 class GlobalMissionPlanner:
     def __init__(self):
@@ -10,6 +11,8 @@ class GlobalMissionPlanner:
 
         # Subscriber for the map
         self.map_sub = rospy.Subscriber('/map', OccupancyGrid, self.map_callback)
+
+        self.robot_amount = int(1)
 
         # Service for the waypoints
         self.waypoints_service = rospy.Service('global_mission/waypoints', PoseArray, self.waypoints_callback)
@@ -25,8 +28,43 @@ class GlobalMissionPlanner:
         self.divide_map_and_generate_waypoints()
 
     def divide_map_and_generate_waypoints(self):
-        # TODO: Implement the logic to divide the map among robots and generate waypoints
-        pass
+        # Get the map dimensions
+        map_width = self.map_data.info.width
+        map_height = self.map_data.info.height
+
+        # Calculate the width of each strip
+        strip_width = map_width // self.robot_amount
+
+        # Initialize the waypoints list
+        waypoints = []
+
+        # Generate waypoints for each robot
+        for i in range(self.robot_amount):
+            # Calculate the strip boundaries
+            strip_start = i * strip_width
+            strip_end = (i + 1) * strip_width if i < self.robot_amount - 1 else map_width
+
+            # Initialize the waypoints for this robot
+            robot_waypoints = []
+
+            # Generate waypoints in a zigzag pattern
+            for y in range(map_height):
+                if y % 2 == 0:  # Even rows go from left to right
+                    x_range = range(strip_start, strip_end)
+                else:  # Odd rows go from right to left
+                    x_range = range(strip_end - 1, strip_start - 1, -1)
+
+                for x in x_range:
+                    # Create a waypoint at (x, y) and add it to the list
+                    waypoint = Pose()
+                    waypoint.position.x = x
+                    waypoint.position.y = y
+                    robot_waypoints.append(waypoint)
+
+            # Add the waypoints for this robot to the main list
+            waypoints.append(robot_waypoints)
+
+        return waypoints
 
     def waypoints_callback(self, request):
         # TODO: Implement the logic to return the waypoints for each robot
