188ae632d5d448a2a82c044fc5fdad9c754dc1a4
Shantnav Agarwal <shantnavagarwal@gmail.com>
2024-06-04 19:37:28 +0000

    WIP

--- mission_planner/src/mission_planner.py
+++ mission_planner/src/mission_planner.py
@@ -8,10 +8,9 @@ class MissionPlanner:
     def __init__(self):
         print("MissionPlanner node initialized")
         self.robot_id : int = None
-        self.path: list = []  # from the global path planner
         self.path_numpy: np.ndarray = None
         self.faeces_location: list = []
-        self.robot_location = None
+        self.robot_posn = None
         self.path_numpy_to_follow = None  # stores the current path to follow
         self.status = None
         self.current_goal = None
@@ -32,12 +31,16 @@ class MissionPlanner:
     @staticmethod
     def pose_array_to_path(pose: PoseArray):
         n = len(pose.poses)
-        pose_numpy = np.empty((n, 4), dtype=np.float64)
+        pose_numpy = np.empty((n, 7), dtype=np.float64)
 
         for i in range(n):
             pose_numpy[i, 0] = pose.poses[i].position.x
             pose_numpy[i, 1] = pose.poses[i].position.y
             pose_numpy[i, 2] = pose.poses[i].position.z
+            pose_numpy[i, 3] = pose.poses[i].orientation.x
+            pose_numpy[i, 4] = pose.poses[i].orientation.y
+            pose_numpy[i, 5] = pose.poses[i].orientation.z
+            pose_numpy[i, 6] = pose.poses[i].orientation.w
         return pose_numpy
     
     @staticmethod
@@ -49,24 +52,32 @@ class MissionPlanner:
         return
 
     def obj_track_callback(self, data: ObjectLocationArray):
-        d = ObjectLocation()
-        d_arr = self.point_to_array(d.abs_location)
-        diff = self.path_numpy - d_arr
-        dist = np.linalg.norm(diff, ord=2, axis=1)
-        min_ind = np.argmin(dist)
-        if min_ind == 0:
-            insert_ind = 1
-        elif min_ind == len(diff) - 1:
-            insert_ind = min_ind - 1
-        else:
-            next_ind = min_ind + 1 if dist[min_ind + 1] < dist[min_ind - 1] else min_ind - 1
-        self.path_numpy = None
+        for d in data.object_location:
+            d_arr = self.point_to_array(d.abs_location)
+            diff = self.path_numpy[:, :3] - d_arr
+            dist = np.linalg.norm(diff, ord=2, axis=1)
+            min_ind = np.argmin(dist)
+            if min_ind == 0:
+                insert_ind = 1
+            elif min_ind == len(diff) - 1:
+                insert_ind = min_ind - 1
+            else:
+                insert_ind = min_ind if dist[min_ind + 1] < dist[min_ind - 1] else min_ind - 1
+            d_arr = np.insert(d_arr, [2], np.zeros_like(4))
+            self.path_numpy = np.insert(self.path_numpy, insert_ind, d_arr)
         return
 
     def own_location_callback(self, data: PoseWithCovarianceStamped):
         position = data.pose.pose.position
         orientation = data.pose.pose.orientation
-        print(position, orientation)
+        self.robot_posn = np.empty((1, 7))
+        self.robot_posn[0] = position.x
+        self.robot_posn[1] = position.y
+        self.robot_posn[2] = position.z
+        self.robot_posn[3] = orientation.x
+        self.robot_posn[4] = orientation.y
+        self.robot_posn[5] = orientation.z
+        self.robot_posn[6] = orientation.w
         return
 
     def battery_callback(self, data: BatteryState):
