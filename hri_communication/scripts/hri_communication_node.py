3c083fae75e2536d4d9c9acc1973f812e5edf55d
tvennink <t.vennink@student.tudelft.nl>
2024-06-05 11:26:46 +0200

    Created full buildable pkg with service required

--- hri_communication/scripts/hri_communication_node.py
+++ hri_communication/scripts/hri_communication_node.py
@@ -1,27 +1,55 @@
 #!/usr/bin/env python
 import rospy
 import roslaunch
-from hri_communication.srv import StartMission
+from srv import StartMapping, StartNavigation, SaveMap
+import subprocess
 
 class HRICommunication:
     def __init__(self):
         rospy.init_node('hri_communication_node')
 
         # Service to start the mission
-        self.start_mission_service = rospy.Service('start_mission', StartMission, self.start_mission_callback)
-
+        self.start_mission_service = rospy.Service('start_mission', StartMapping, self.start_mission_callback)
+        # Service to start navigation
+        self.start_navigation_service = rospy.Service('start_navigation', StartNavigation,
+                                                      self.start_navigation_callback)
+        # Service to save the map
+        self.save_map_service = rospy.Service('save_map', SaveMap, self.save_map_callback)
     def start_mapping_callback(self, request):
         # Generate a unique identifier for the launch
         uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
         roslaunch.configure_logging(uuid)
 
         # Create a ROSLaunchParent object
-        launch = roslaunch.parent.ROSLaunchParent(uuid, ["/path/to/mapping.launch"])
+        launch = roslaunch.parent.ROSLaunchParent(uuid, ["navigation/launch/mapping.launch"])
 
         # Start the launch file
         launch.start()
 
         return []
+
+    def start_navigation_callback(self, request):
+        # Generate a unique identifier for the launch
+        uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+        roslaunch.configure_logging(uuid)
+
+        # Create a ROSLaunchParent object
+        launch = roslaunch.parent.ROSLaunchParent(uuid, ["navigation/launch/navigation.launch"])
+
+        # Start the launch file
+        launch.start()
+
+        return []
+
+def save_map_callback(self, request):
+    # Define the command as a list of strings
+    command = ["rosrun", "map_server", "map_saver", "-f", "/path/to/save/map"]
+
+    # Run the command
+    subprocess.run(command)
+
+    return []
+
 
 if __name__ == '__main__':
     try:
