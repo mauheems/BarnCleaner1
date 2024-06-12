6bcb531fc035cd32fd7ad86ee7259663659aafdb
tvennink <t.vennink@student.tudelft.nl>
2024-06-12 12:10:02 +0200

    hri_communication refactor _node.py

--- hri_communication/setup.py
+++ hri_communication/setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['hri_communication'],
-    scripts=['scripts/hri_communication'],
+    packages=['hri_communication_node.py'],
+    scripts=['scripts/hri_communication_node.py'],
     package_dir={'':'src'}
 )
 
