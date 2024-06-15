b98ad934ef1cd1ad419d9e635d4760eddd5969b4
Shantnav Agarwal <shantnavagarwal@gmail.com>
2024-06-15 16:04:41 +0200

    Added documentation

--- object_detector/src/custom_msg_util.py
+++ object_detector/src/custom_msg_util.py
@@ -8,12 +8,33 @@ import numpy as np
 
 class CustomMsgUtil(object):
     def __init__(self):
+        """
+        Initializes the CustomMsgUtil class.
+
+        This constructor initializes the CustomMsgUtil class by setting up the necessary publishers and subscribers.
+        It creates a CvBridge instance to convert ROS messages to OpenCV images and vice versa.
+        It subscribes to the "/object_detector/detections" topic of type Detection and calls the publish_img method when a new message is received.
+        It creates two publishers: "/object_detector/annotated_detections" of type Image and "/object_detector/depth_detections" of type Image.
+
+        This constructor does not take any parameters.
+
+        This constructor does not return any values.
+        """
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/object_detector/detections", Detection, self.publish_img)
         self.rgb_pub = rospy.Publisher("/object_detector/annotated_detections", Image, queue_size=1)
         self.depth_pub = rospy.Publisher("/object_detector/depth_detections", Image, queue_size=1)
         
     def publish_img(self, msg: Detection):
+        """
+        Publishes the annotated RGB and Depth images based on the Detection message received.
+        
+        Parameters:
+            msg (Detection): The Detection message containing source images and bounding box detections.
+
+        Returns:
+            None
+        """
         rgb_img = self.bridge.imgmsg_to_cv2(msg.source_img, desired_encoding='bgr8')
         depth_img = self.bridge.imgmsg_to_cv2(msg.depth_img, desired_encoding='16UC1')
 
